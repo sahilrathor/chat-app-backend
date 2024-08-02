45436eb7f0231b4b9e404c52113634f9393096a1
Sahil Rathor <sahil@smarden.in>
2024-08-02 23:19:19 +0530

    cors methods updated

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors({
     origin: "http://localhost:5173",
     credentials: true,
-    sameSite: "strict"
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE'
 }));
 
 app.use("/api/auth", authRoutes);
